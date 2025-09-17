0ac5d78d1d4bf80bd1f5ca6fbd7ad8c1e91f5d4a
novasup666 <noe.vincent@protonmail.com>
2025-09-17 09:00:32 +0200

    part 5 completed

--- homework_02/main.py
+++ homework_02/main.py
@@ -1,21 +1,45 @@
-file_names = [f"homework_02/downloads/file{i}.fa" for i in range(1,7)]
-def ns_csl(file_name):
+N = 7
+
+file_names = [f"homework_02/downloads/file{i}.fa" for i in range(1,N)]
+
+
+
+def ns_csl_seq(file_name):
     f = open(file_name,"r")
     nb_seq = 0
+    sequences = []
     cumulative_seq_length = 0
     l = f.readline()
     while l!="":
         if l[0]==">":
             nb_seq+=1
+            sequences.append([])
         else:
             cumulative_seq_length+=len(l)
+            if l[-1] == "\n":
+                sequences[-1].append(l[:-1])
+            else:
+                sequences[-1].append(l)
         l = f.readline()
-    return(nb_seq,cumulative_seq_length)
+    seq = ["".join(s) for s in sequences]
+    return(nb_seq,cumulative_seq_length,seq)
+
 
 
 def cano_kmers(sequences,k):
     cano_kmers = set()
-    def canonize(kmer):
+
+    def canonize(k,kmer):
+        num_kmer = 0
+        num_comp = 0
+        table = {"A" : 0,"C":1,"G":2,"T":3}
+        comptable = {"A":3,"C":2,"G":1,"T":0}
+        for i in range(k):
+            num_kmer = 4*num_kmer + table[kmer[i]]
+            num_comp += comptable[kmer[i]]*(4**i)
+        return min(num_kmer,num_comp)
+
+    """def canonize(kmer):
         candidate = []
         for c in kmer:
             if c == "A":
@@ -26,14 +50,35 @@ def cano_kmers(sequences,k):
                 candidate.append("G")            
             if c == "G":
                 candidate.append("T")
-        return min(kmer,candidate)
-
+        
+        candidate = "".join(candidate[::-1])
+       
+        return numerize_kmer(k,min(kmer,candidate))
+"""
     for s in sequences : 
         for i in range(len(s)-k):
-            cano_kmers.add(canonize(s[i:i+k]))
-    return list(cano_kmers)
+            kmer = s[i:i+k]
+            if "N" not in kmer : 
+                cano_kmers.add(canonize(k,kmer))
+    return cano_kmers
+
+K = 20
+
+ckmer_sets = []
 
 for name in file_names:
-    (n,l) = ns_csl(name)
+    (n,l,seq) = ns_csl_seq(name)
     print(f"{name}: {n} sequences of total length {l}")
+    ckmer_sets.append(cano_kmers(seq,K))
 
+jaccardMatrix  = [[0 for _ in range(1,7)] for _ in range(1,N)]
+
+for i in range(N-1) :
+    for j in range(i,N-1) :
+        a = len(ckmer_sets[i].union(ckmer_sets[j]))
+        b = len(ckmer_sets[i].intersection(ckmer_sets[j]))
+        jaccardMatrix[i][j] = b/a
+
+print("The pairwise Jaccard indexes are :")
+for l in jaccardMatrix:
+    print(l)
